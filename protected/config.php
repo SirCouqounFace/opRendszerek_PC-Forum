dc445cd11e9d38904ad12d18422ed3db366f1412
SirCouqounFace <61544417+SirCouqounFace@users.noreply.github.com>
2021-01-12 21:34:34 +0100

    Update config.php

--- protected/config.php
+++ protected/config.php
@@ -4,7 +4,7 @@ define('PUBLIC_DIR', BASE_DIR.'public/');
 define('PROTECTED_DIR', BASE_DIR.'protected/');
 
 define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
-define('USER_MANAGER', PROTECTED_DIR.'userManager.php');
+define('USER_MANAGER', PROTECTED_DIR. 'userManager.php');
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'sql7.freemysqlhosting.net');
