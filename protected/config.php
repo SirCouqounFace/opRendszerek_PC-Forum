d14d1c345881825f9198e24ded4fba07019c622b
SirCouqounFace <61544417+SirCouqounFace@users.noreply.github.com>
2021-01-12 21:16:34 +0100

    Update config.php

--- protected/config.php
+++ protected/config.php
@@ -7,9 +7,9 @@ define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 define('USER_MANAGER', PROTECTED_DIR.'userManager.php');
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'remotemysql.com');
-define('DB_NAME', 'jqzwveaYml');
-define('DB_USER', 'jqzwveaYml');
-define('DB_PASS', 'ZATjsJdLsL');
+define('DB_HOST', 'sql7.freemysqlhosting.net');
+define('DB_NAME', 'sql7386619');
+define('DB_USER', 'sql7386619');
+define('DB_PASS', 'wpRfVMWpmA');
 define('DB_CHARSET', 'utf8');
 ?>
