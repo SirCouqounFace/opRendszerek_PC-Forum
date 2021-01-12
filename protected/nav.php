2a014ba90d9de1d64241d2cb4bdbce2c7b3d489e
SirCouqounFace <61544417+SirCouqounFace@users.noreply.github.com>
2021-01-12 21:45:13 +0100

    Update nav.php

--- protected/nav.php
+++ protected/nav.php
@@ -1,19 +1,19 @@
 <hr>
 
 <a href="index.php">Home</a>
-	<span> &nbsp; | &nbsp; </span>
-	<a href="index.php?P=login">Login</a>
-	<span> &nbsp; | &nbsp; </span>
-	<a href="index.php?P=register">Register</a>
-	<span> &nbsp; || &nbsp; </span>
-	<a href="index.php?P=users">User List</a>
-	<span> &nbsp; | &nbsp; </span>		
-	<span> &nbsp; | &nbsp; </span>
-	<a href="index.php?P=list_topic">List Topics</a>
-	<span> &nbsp; | &nbsp; </span>
-	<a href="index.php?P=add_topic">Start Topic</a>
-	<span> &nbsp; || &nbsp; </span>
-	<a href="index.php?P=logout">Logout</a>
-<?php endif; ?>
+<span> &nbsp; | &nbsp; </span>
+<a href="index.php?P=login">Login</a>
+<span> &nbsp; | &nbsp; </span>
+<a href="index.php?P=register">Register</a>
+<span> &nbsp; || &nbsp; </span>
+<a href="index.php?P=users">User List</a>
+<span> &nbsp; | &nbsp; </span>		
+<span> &nbsp; | &nbsp; </span>
+<a href="index.php?P=list_topic">List Topics</a>
+<span> &nbsp; | &nbsp; </span>
+<a href="index.php?P=add_topic">Start Topic</a>
+<span> &nbsp; || &nbsp; </span>
+<a href="index.php?P=logout">Logout</a>
+
 
 <hr>
